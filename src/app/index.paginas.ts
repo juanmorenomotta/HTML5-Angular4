b5256c52dc4ce40008407ba7bff3bfdefced45a6
Juan Moreno <juanmorenomotta@gmail.com>
2017-08-24 02:05:54 -0500

    Rutas agregadas

---
+++ src/app/index.paginas.ts
@@ -0,0 +1,4 @@
+
+export { AboutComponent } from "./components/about/about.component";
+export { PortafolioComponent } from "./components/portafolio/portafolio.component";
+export { ItemComponent } from "./components/item/item.component";
